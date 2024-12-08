49699ca8012cf845ae0e6e1c659f19910e88ca0a
Tim Mitra <tim@it-guy.com>
2024-12-08 02:38:17 -0500

    Initial Commit

---
+++ SwiftDataUIkit2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+// ---------------------------- //
+// Original Project: SwiftDataUIkit2
+// Created on 2024-12-08 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
